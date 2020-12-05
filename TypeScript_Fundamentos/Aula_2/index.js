2a37b8c0417239e2eebb9b3ffcf54a0475e1fdf1
lordefps <lodizinho_mice@hotmail.com>
2020-12-05 17:12:20 -0300

    Json

--- TypeScript_Fundamentos/Aula_2/index.js
+++ TypeScript_Fundamentos/Aula_2/index.js
@@ -1,11 +1,9 @@
 var input1 = document.getElementById("num_1");
 var input2 = document.getElementById("num_2");
 var button = document.getElementById("button");
-
 function sum(a, b) {
     return a + b;
 }
-
 button.addEventListener("click", function () {
     console.log(sum(Number(input1.value), Number(input2.value)));
 });
