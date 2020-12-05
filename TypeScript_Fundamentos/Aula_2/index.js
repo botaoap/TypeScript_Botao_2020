feffe6744a4ca614556654757e4ddebdea7f2011
lordefps <lodizinho_mice@hotmail.com>
2020-12-05 16:51:42 -0300

    Compilacao_TS_to_JS

--- TypeScript_Fundamentos/Aula_2/index.js
+++ TypeScript_Fundamentos/Aula_2/index.js
@@ -1,13 +1,11 @@
-const input1 = document.getElementById("num_1");
-const input2 = document.getElementById("num_2");
-const button = document.getElementById("button");
+var input1 = document.getElementById("num_1");
+var input2 = document.getElementById("num_2");
+var button = document.getElementById("button");
 
-function sum(a,b){
+function sum(a, b) {
     return a + b;
 }
 
-button.addEventListener("click", function(){
-    
-    console.log(sum(input1.value, input2.value));
-
-})
+button.addEventListener("click", function () {
+    console.log(sum(Number(input1.value), Number(input2.value)));
+});
