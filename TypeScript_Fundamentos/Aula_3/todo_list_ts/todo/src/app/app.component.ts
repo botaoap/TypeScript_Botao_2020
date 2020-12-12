ed85c0b1ad01a4a2fdc9990f59c71ae52b129600
lordefps <lodizinho_mice@hotmail.com>
2020-12-12 17:48:18 -0300

    Criando_Construtor

--- TypeScript_Fundamentos/Aula_3/todo_list_ts/todo/src/app/app.component.ts
+++ TypeScript_Fundamentos/Aula_3/todo_list_ts/todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +8,30 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public todos : any[]  = [];
+  public todos : Todo[]  = [];
   public title : String = 'Minhas tarefas';
   public nome : String = 'Gabriel'
 
   constructor() { // contrutor: primeiro método para executar
 
+    /*
     this.todos.push('passear com cachorro');
     this.todos.push('ir ao supermercado');
     this.todos.push('cortar cabelo');
-    /*
+    
     this.todos.push(1999);
     this.todos.push([{message : 'teste'}]);
     this.todos.push(new Date());
     */
+
+    // pode ser feito passando o objeto como a linha  abaixo,
+    // mas iremos usar o construtor da classe
+    //this.todos.push(new Todo(), {id : 2, todo : 'Levar o cachorro para passear', done : false});
+    // ____ // 
+    // ↓↓ Usando o contrutor para preencher ↓↓
+    this.todos.push(new Todo(1, 'Passear com o charro', false));
+    this.todos.push(new Todo(2, 'Ir ao cupermercado', true));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', false));
 
   }
 
