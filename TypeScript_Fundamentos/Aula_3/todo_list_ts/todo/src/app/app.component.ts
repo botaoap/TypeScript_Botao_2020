5288ffdab2ccdb9273ac8a5c2b91ef0863b57ebb
lordefps <lodizinho_mice@hotmail.com>
2020-12-12 17:04:54 -0300

    Site_todos

--- TypeScript_Fundamentos/Aula_3/todo_list_ts/todo/src/app/app.component.ts
+++ TypeScript_Fundamentos/Aula_3/todo_list_ts/todo/src/app/app.component.ts
@@ -9,17 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public todos : any[]  = [];
   public title : String = 'Minhas tarefas';
+  public nome : String = 'Gabriel'
 
-  constructor() { // contrutor executa 
+  constructor() { // contrutor: primeiro método para executar
 
     this.todos.push('passear com cachorro');
     this.todos.push('ir ao supermercado');
     this.todos.push('cortar cabelo');
+    /*
+    this.todos.push(1999);
+    this.todos.push([{message : 'teste'}]);
+    this.todos.push(new Date());
+    */
 
   }
 
   alterarTexto() {
 
     this.title = 'Texto alterado'
+    this.nome = 'Gabriel Botão'
   }
 }
